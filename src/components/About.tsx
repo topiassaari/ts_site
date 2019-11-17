43ec8922afb60357aedeafaec1b743599b34240d
Topias Saari <topias@Topiass-iMac.local>
2019-11-17 14:26:01 +0200

    initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/App.scss";
+import Naama from "../assets/ts_naama.jpeg";
+
+class About extends React.Component {
+  render() {
+    return (
+      <article className="aboutContainer">
+        <div className="aboutText">
+          <img src={Naama} id="face" alt="naama" />
+          <p>
+            Located in Helsinki, Finland. Currently working on a design system at ABB as a web developer.
+            <br />
+            <br />
+            Packed and shipped frequency converters at a warehouse for a few years, then got a BBA from Haaga-Helia
+            University of Applied Sciences with a focus on user centered design. Spent half a year in an exchange
+            programme at Hanze in Groningen, Netherlands learning how to make video-games. Wrote a
+            <a href="https://www.theseus.fi/handle/10024/227894"> thesis on design tokens.</a>
+            <br />
+            <br />
+            Formerly a part of Studio Kraft - a professional photo-team for business events and private occasions such
+            as graduations. Former clients include Nordic Business Forum and LOGY Ry.
+            <br />
+            <br />
+            As a passion project learning to draw and make pretty pictures.
+            <a href="https://www.instagram.com/topias/">@topias</a> on instagram.
+          </p>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Projects.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "../styles/App.scss";
+
+class Projects extends React.Component {
+  render() {
+    return (
+      <article>
+        <section className="links">
+          <b>Web</b>
+          <a href="http://ding-me.com/">Ding</a>
+
+          <a href="https://topiassaari.github.io/dots/">Dots</a>
+
+          <a href="https://topiassaari.github.io/is_it_seven/">Is it seven?</a>
+
+          <b>Games</b>
+          <a href="https://www.woodwardfoliporto.nl/game-design/portable-escape-room-2018">
+            Protocol: Bird Cage
+          </a>
+
+          <a href="https://www.youtube.com/watch?v=BqmUf1abX8E&feature=emb_title">
+            The Left Hand
+          </a>
+
+          <b>Haaga-Helia UX-projects</b>
+          <a href="https://hayleygrant.com/project/04d3af27/Planty">Planty</a>
+          <a href="https://hayleygrant.com/project/04666e62/From-Foreign--to-Familiar">
+            From Foreign to Familiar
+          </a>
+          <a href="https://hayleygrant.com/project/04fc8976/Shelf">Shelf</a>
+
+          <b>Videos</b>
+          <a href="https://youtu.be/Lc1hmKSKrv4">
+            Rinse & Repeat - Riton ft. Kah-Lo | Nelli Karmila Choreography
+          </a>
+
+          <a href="https://youtu.be/JepI20GtaL8">
+            Entäs Peltonen? - Nurkkapöytä (Virallinen musiikkivideo)
+          </a>
+
+          <a href="https://youtu.be/ZzYLEzxPkQA">BAK - 3+4=6</a>
+          <a href="https://youtu.be/fJXt_spPHsY">
+            Michael Shié - Kelloo / Niimonii
+          </a>
+          <a href="https://youtu.be/eitaG4bi-RU">
+            Lauantai - Olkkarin sohvalta etelään (musiikkivideo)
+          </a>
+
+          <a href="https://www.youtube.com/watch?v=nh9P5Kfq0ng">
+            Games 2017: ValoClimb® – Interactive game platform for climbing
+            walls | Valo Motion
+          </a>
+          <a href="https://youtu.be/8phbX6HgFEY">
+            Pikseli Arcade - Koe mitä haluat
+          </a>
+
+          <span>
+            <a href="https://youtu.be/rVZzQGRTT7c">
+              Poets of the Fall - Moonlight Kissed (Official 4K Video w/ Lyrics)
+            </a>
+            - As a grip assistant / runner
+          </span>
+        </section>
+      </article>
+    );
+  }
+}
+
+export default Projects;
