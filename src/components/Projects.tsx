9d1f7d1d46b3f9a7144e75e2e211aaaafc891968
topias <saari.topias@gmail.com>
2020-05-13 09:01:05 +0000

    format fixes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,57 +6,135 @@ class Projects extends React.Component {
     return (
       <article>
         <section className="links">
-        <b>Current</b>
-	<ul>
-		<li><span><a href="https://www.idean.com/work/abb-humanizing-technology-with-design">ABB CommonUX Design System (article by Idean)</a> - as a junior web developer for ABB</span></li>
-	</ul>  
-	<b>Web</b>
-	<ul>
-        	 <li><a href="http://ding-me.com/">Ding</a></li>
-        	 <li><a href="https://topiassaari.github.io/dots/">Dots</a></li>
-        	 <li><a href="https://topiassaari.github.io/is_it_seven/">Is it seven?</a></li>
-	</ul>
-	<b>Games</b>
-	<ul>     
-   		<li><a href="https://www.woodwardfoliporto.nl/game-design/portable-escape-room-2018"> Protocol: Bird Cage</a></li>
-    	 	<li><a href="https://www.youtube.com/watch?v=BqmUf1abX8E&feature=emb_title">The Left Hand</a></li>
-	</ul>
-         <b>Haaga-Helia UX-projects</b>
-        <ul>
-		<li><a href="https://hayleygrant.com/project/04d3af27/Planty">Planty</a></li>
-         	<li><a href="https://hayleygrant.com/project/04666e62/From-Foreign--to-Familiar">From Foreign to Familiar</a></li>
-         	<li><a href="https://hayleygrant.com/project/04fc8976/Shelf">Shelf</a></li>
-	</ul>
+          <b>Current</b>
+          <ul>
+            <li>
+              <span>
+                <a href="https://www.idean.com/work/abb-humanizing-technology-with-design">
+                  ABB CommonUX Design System (article by Idean)
+                </a>{" "}
+                - as a junior web developer for ABB
+              </span>
+            </li>
+          </ul>
+          <b>Web</b>
+          <ul>
+            <li>
+              <a href="http://ding-me.com/">Ding</a>
+            </li>
+            <li>
+              <a href="https://topiassaari.github.io/dots/">Dots</a>
+            </li>
+            <li>
+              <a href="https://topiassaari.github.io/is_it_seven/">
+                Is it seven?
+              </a>
+            </li>
+          </ul>
+          <b>Games</b>
+          <ul>
+            <li>
+              <a href="https://www.woodwardfoliporto.nl/game-design/portable-escape-room-2018">
+                {" "}
+                Protocol: Bird Cage
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/watch?v=BqmUf1abX8E&feature=emb_title">
+                The Left Hand
+              </a>
+            </li>
+          </ul>
+          <b>Haaga-Helia UX-projects</b>
+          <ul>
+            <li>
+              <a href="https://hayleygrant.com/project/04d3af27/Planty">
+                Planty
+              </a>
+            </li>
+            <li>
+              <a href="https://hayleygrant.com/project/04666e62/From-Foreign--to-Familiar">
+                From Foreign to Familiar
+              </a>
+            </li>
+            <li>
+              <a href="https://hayleygrant.com/project/04fc8976/Shelf">Shelf</a>
+            </li>
+          </ul>
           <b>Photography</b>
-	<ul>        
-  		<li><span><a href="https://www.nbforum.com/past-events/nordic-business-forum-2016/">Nordic Business Forum (2014-2016)</a>- with Studio Kraft</span></li>
-         	<li><a href="https://uskallayrittaa.fi/suomen-parhaat-nuoret-yrittajat-palkittiin-uskalla-yrittaa-finaalissa-etela-pohjanmaalle-kaksoisvoitto/">NY Uskalla yrittää - finaali (2015)</a></li>
-		<li><a href="https://www.soundi.fi/jutut/nuoret-ja-raikkaat-for-tomorrow/">Nuoret ja raikkaat: For Tomorrow</a></li>
-          	<li><a href="https://www.fullsteam.fi/fi/artistit/entas-peltonen">Fullsteam Agency: Entäs Peltonen?</a></li>
-	</ul>
+          <ul>
+            <li>
+              <span>
+                <a href="https://www.nbforum.com/past-events/nordic-business-forum-2016/">
+                  Nordic Business Forum (2014-2016)
+                </a>
+                - with Studio Kraft
+              </span>
+            </li>
+            <li>
+              <a href="https://uskallayrittaa.fi/suomen-parhaat-nuoret-yrittajat-palkittiin-uskalla-yrittaa-finaalissa-etela-pohjanmaalle-kaksoisvoitto/">
+                NY Uskalla yrittää - finaali (2015)
+              </a>
+            </li>
+            <li>
+              <a href="https://www.soundi.fi/jutut/nuoret-ja-raikkaat-for-tomorrow/">
+                Nuoret ja raikkaat: For Tomorrow
+              </a>
+            </li>
+            <li>
+              <a href="https://www.fullsteam.fi/fi/artistit/entas-peltonen">
+                Fullsteam Agency: Entäs Peltonen?
+              </a>
+            </li>
+          </ul>
           <b>Videos</b>
-	<ul>
-          	<li><a href="https://youtu.be/Lc1hmKSKrv4">Rinse & Repeat - Riton ft. Kah-Lo | Nelli Karmila Choreography</a></li>
-          	<li><a href="https://youtu.be/JepI20GtaL8">Entäs Peltonen? - Nurkkapöytä (Virallinen musiikkivideo)</a></li>
-		<li><a href="https://youtu.be/ZzYLEzxPkQA">BAK - 3+4=6</a></li>
-          	<li><a href="https://youtu.be/fJXt_spPHsY">Michael Shié - Kelloo / Niimonii</a></li>
-		<li><a href="https://youtu.be/eitaG4bi-RU">Lauantai - Olkkarin sohvalta etelään (musiikkivideo)</a></li>
+          <ul>
+            <li>
+              <a href="https://youtu.be/Lc1hmKSKrv4">
+                Rinse &amp; Repeat - Riton ft. Kah-Lo | Nelli Karmila
+                Choreography
+              </a>
+            </li>
+            <li>
+              <a href="https://youtu.be/JepI20GtaL8">
+                Entäs Peltonen? - Nurkkapöytä (Virallinen musiikkivideo)
+              </a>
+            </li>
+            <li>
+              <a href="https://youtu.be/ZzYLEzxPkQA">BAK - 3+4=6</a>
+            </li>
+            <li>
+              <a href="https://youtu.be/fJXt_spPHsY">
+                Michael Shié - Kelloo / Niimonii
+              </a>
+            </li>
+            <li>
+              <a href="https://youtu.be/eitaG4bi-RU">
+                Lauantai - Olkkarin sohvalta etelään (musiikkivideo)
+              </a>
+            </li>
 
-          <li><a href="https://www.youtube.com/watch?v=nh9P5Kfq0ng">
-            Games 2017: ValoClimb® – Interactive game platform for climbing
-            walls | Valo Motion
-          </a></li>
-          <li><a href="https://youtu.be/8phbX6HgFEY">
-            Pikseli Arcade - Koe mitä haluat
-          </a></li>
-<li>
-          <span>
-            <a href="https://youtu.be/rVZzQGRTT7c">
-              Poets of the Fall - Moonlight Kissed (Official 4K Video w/ Lyrics)
-            </a>
-            - As a grip assistant / runner
-          </span></li>
-</ul>
+            <li>
+              <a href="https://www.youtube.com/watch?v=nh9P5Kfq0ng">
+                Games 2017: ValoClimb® – Interactive game platform for climbing
+                walls | Valo Motion
+              </a>
+            </li>
+            <li>
+              <a href="https://youtu.be/8phbX6HgFEY">
+                Pikseli Arcade - Koe mitä haluat
+              </a>
+            </li>
+            <li>
+              <span>
+                <a href="https://youtu.be/rVZzQGRTT7c">
+                  Poets of the Fall - Moonlight Kissed (Official 4K Video w/
+                  Lyrics)
+                </a>
+                - As a grip assistant / runner
+              </span>
+            </li>
+          </ul>
         </section>
       </article>
     );
