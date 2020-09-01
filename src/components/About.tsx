3b2e499cad5180724d8af4def261b36c95b32f59
Topias Saari <topias@Topiass-iMac.local>
2020-09-01 14:54:01 +0300

    update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.scss";
-import Naama from "../assets/ts_naama2.jpeg";
+import Naama from "../assets/ts_naama.jpeg";
 
 class About extends React.Component {
   render() {
@@ -9,8 +9,10 @@ class About extends React.Component {
         <div className="aboutText">
           <img src={Naama} id="face" alt="naama" />
           <p>
-            Located in Helsinki, Finland. Currently working on a design system
-            at ABB as a web developer.
+            Located in Helsinki, Finland. Currently studying for a masters
+            degree in Aalto University with a major in software and service
+            engineering. Also working on a design system at ABB as a web
+            developer.
             <br />
             <br />
             Packed and shipped frequency converters at a warehouse for a few
